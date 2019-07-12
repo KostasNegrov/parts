3087298a7d15c7cc9028920add76a1818ef08a8e
Negrov Konstantin <Kostas29n@gmail.com>
2019-07-12 16:08:43 +0300

    Добавил активные ссылки и прокрутку

--- elements/scroll/main.js
+++ elements/scroll/main.js
@@ -1,16 +1,26 @@
-$(document).ready(function(){
-            
-    $("#nav-menu-container").on("click","a", function (event) { // <nav id="nav-menu-container">
-        //отменяем стандартную обработку нажатия по ссылке
-        event.preventDefault();
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
+$(document).ready(function () {
 
-        top = $(id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
+  $("#nav-menu-container").on("click", "a", function (event) { // <nav id="nav-menu-container">
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
 
-    });
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
 
+$('.nav-menu a').on('click', function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+
+    if ($(this).parents('.nav-menu').length) {
+      $('.nav-menu .menu-active').removeClass('menu-active');
+      $(this).closest('li').addClass('menu-active');
+    }
+
+  }
 });
